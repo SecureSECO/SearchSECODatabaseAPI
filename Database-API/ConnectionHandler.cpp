32b6039939e36e42fea9b722bd0b09472545f840
ilan hartskeerl <ilan@hartskeerl.nl>
2021-03-24 16:13:09 +0100

    Devided the Connection handler up in a header and cpp file, and fixed that it will now work even when both the request and data packets arive before we read

--- Database-API/ConnectionHandler.cpp
+++ Database-API/ConnectionHandler.cpp
@@ -1,45 +1,44 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <boost/bind/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/asio.hpp>
-#include <boost/array.hpp>
-#include "RequestHandler.h"
+
 #include "ConnectionHandler.h"
 
-using boost::asio::ip::tcp;
 
-class tcp_connection
-  : public boost::enable_shared_from_this<tcp_connection>
+
+// Connection Handler Methods
+void ConnectionHandler::StartListen()
 {
-public:
-  typedef boost::shared_ptr<tcp_connection> pointer;
 
-  static pointer create(boost::asio::io_context& io_context)
-  {
-    return pointer(new tcp_connection(io_context));
-  }
+	try
+	{
+		boost::asio::io_context io_context;
+		tcp_server server(io_context);
+		io_context.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
 
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
 
-  void start(RequestHandler handler)
-  {
+// TCP Connection Methods 
+tcp_connection::pointer tcp_connection::create(boost::asio::io_context& io_context)
+{
+	return pointer(new tcp_connection(io_context));
+}
+
+void tcp_connection::start(RequestHandler handler)
+{
 
 	/*boost::asio::async_read(socket_, boost::asio::buffer(request),
 		boost::bind(&tcp_connection::handle_read, shared_from_this(),
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));*/
-	std::vector<char> request(128);
+		  boost::asio::placeholders::error,
+		  boost::asio::placeholders::bytes_transferred));*/
+	std::vector<char> request = std::vector<char>();
 	boost::system::error_code error;
 
-	size_t len = socket_.read_some(boost::asio::buffer(request), error);
+	size_t len = boost::asio::read_until(socket_, boost::asio::dynamic_buffer(request), '\n');
 
-	std::string r(request.begin(), request.end());
+	std::string r(request.begin(), request.begin() + len - 1);
 
 	//size_t* length;
 	//sscanf(r.c_str(), "%zu", length);
@@ -48,88 +47,53 @@ public:
 
 	std::string length = r.substr(4);
 
-	int size = stoi(length);
-
+	int size = stoi(length) - (request.size() - len);
 	std::vector<char> data(size);
-
-	socket_.read_some(boost::asio::buffer(data), error);
+	if (size > 0)
+	{
+		socket_.read_some(boost::asio::buffer(data), error);
+	}
 
 	std::string d(data.begin(), data.end());
 
-	std::string result = handler.HandleRequest(r.substr(0, 3), d);
+	std::string result = handler.HandleRequest(r.substr(0, 4), std::string(request.begin() + len, request.end()) + d);
 
 	boost::asio::write(socket_, boost::asio::buffer(result), error);
 
-    /*boost::asio::async_write(socket_, boost::asio::buffer(result),
-        boost::bind(&tcp_connection::handle_read, shared_from_this(),
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));*/
-  }
+	/*boost::asio::async_write(socket_, boost::asio::buffer(result),
+		boost::bind(&tcp_connection::handle_read, shared_from_this(),
+		  boost::asio::placeholders::error,
+		  boost::asio::placeholders::bytes_transferred));*/
+}
 
-private:
-  tcp_connection(boost::asio::io_context& io_context)
-    : socket_(io_context)
-  {
-  }
 
-  void handle_read(const boost::system::error_code& /*error*/,
-      size_t /*bytes_transferred*/)
-  {
-  }
+// TCP server Methods
 
-  tcp::socket socket_;
-  std::string message_;
-};
-
-class tcp_server
+tcp_server::tcp_server(boost::asio::io_context& io_context)
+	: io_context_(io_context),
+	acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
 {
-public:
-  tcp_server(boost::asio::io_context& io_context)
-    : io_context_(io_context),
-      acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
-  {
 	handler.Initialize();
-    start_accept();
-  }
+	start_accept();
+}
 
-private:
-  void start_accept()
-  {
-    tcp_connection::pointer new_connection =
-      tcp_connection::create(io_context_);
-
-    acceptor_.async_accept(new_connection->socket(),
-        boost::bind(&tcp_server::handle_accept, this, new_connection,
-          boost::asio::placeholders::error));
-  }
-
-  void handle_accept(tcp_connection::pointer new_connection,
-      const boost::system::error_code& error)
-  {
-    if (!error)
-    {
-      new_connection->start(handler);
-    }
-
-    start_accept();
-  }
-
-  boost::asio::io_context& io_context_;
-  tcp::acceptor acceptor_;
-	RequestHandler handler;
-};
-
-void ConnectionHandler::StartListen()
+void tcp_server::start_accept()
 {
+	tcp_connection::pointer new_connection =
+		tcp_connection::create(io_context_);
 
-  try
-  {
-    boost::asio::io_context io_context;
-    tcp_server server(io_context);
-    io_context.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
+	acceptor_.async_accept(new_connection->socket(),
+		boost::bind(&tcp_server::handle_accept, this, new_connection,
+			boost::asio::placeholders::error));
+}
+
+void tcp_server::handle_accept(tcp_connection::pointer new_connection,
+	const boost::system::error_code& error)
+{
+	start_accept();
+	if (!error)
+	{
+		new_connection->start(handler);
+	}
+
 }
