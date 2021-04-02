189a3d94163c0b60d8e0a3774d7ddd8034b085b0
Marien <m.p.matser@students.uu.nl>
2021-04-02 14:47:47 +0200

    Cleaned up main method

--- Database-API/Database-API.cpp
+++ Database-API/Database-API.cpp
@@ -14,41 +14,8 @@ using namespace std;
 
 int main()
 {
-	DatabaseHandler database;
-	database.Connect();
-
-	/*Project project;
-	project.projectID = "de485100-2a23-4373-a308-b33378a6e6be";
-	project.version = time(0);
-	project.license = "Free";
-	project.name = "Test";
-	project.url = "test.test";
-	Author owner;
-	owner.name = "Mister owner";
-	owner.mail = "owner@test.test";
-	project.owner = owner;
-	project.stars = 69;
-	vector<string> hashes;
-	hashes.push_back("de485100-2a23-4373-a308-b33378a6e6be");
-	project.hashes = hashes;
-
-	Method method;
-	method.hash = "de485100-2a23-4373-a308-b33378a6e6be";
-	method.methodName = "NiceTestMethod";
-	method.fileLocation = "Great/File/Location.c";
-	vector<Author> authors;
-	authors.push_back(owner);
-	method.authors = authors;
-
-	cout << "Adding project" << endl;*/
-
-	//vector<Method> methods = database.HashToMethods("de485100-2a23-4373-a308-b33378a6e6be");
-
 	ConnectionHandler listen;
 	listen.StartListen();
 
-	//cout << methods[0].hash << endl;
-
-	cout << "Hello CMake." << endl;
 	return 0;
 }
