7af90ab8d59f92b486110464cf6275acab051e4b
Joël Ganesh <j.ganesh@students.uu.nl>
2021-04-07 17:52:30 +0200

    The first UploadRequest test should now actually be working.

--- Database-API_Test/UploadRequest_tests.cpp
+++ Database-API_Test/UploadRequest_tests.cpp
@@ -1,16 +1,60 @@
 #include "RequestHandler.h"
 #include "DatabaseMock.cpp"
 #include <gtest/gtest.h>
+#include <vector>
+
+using namespace std;
 
 /*TEST(UploadRequest, EmptyRequest)
 {
     RequestHandler handler;
-    EXPECT_EQ(handler.handleUploadRequest("upld"), );
+    EXPECT_EQ(handler.handleUploadRequest("upld", ""), );
 }*/
+
+MATCHER_P(projectEqual, project, "")
+{
+return arg.projectID  == project.projectID
+    && arg.version    == project.version
+    && arg.license    == project.license
+    && arg.name       == project.name
+    && arg.url        == project.url
+    && arg.owner.name == project.owner.name
+    && arg.owner.mail == project.owner.mail
+    && arg.stars      == project.stars
+    && arg.hashes     == project.hashes;
+}
+
+MATCHER_P(methodEqual, method, "")
+{
+	return arg.hash         == method.hash
+            && arg.methodName   == method.methodName
+            && arg.fileLocation == method.fileLocation
+            && arg.lineNumber   == method.lineNumber;
+}
+
+MATCHER_P(ownersEqual, owners, "")
+{
+        if (arg.authors.size() != owners.size())
+        {
+                return false;
+        }
+        else
+        {
+                for (int i = 0; i < owners.size(); i++)
+                {
+                        if (arg.authors[i].name != owners[i].name || arg.authors[i].mail != owners[i].mail)
+                        {
+                                return false;
+                        }
+                }
+		return true;
+	}
+}
 
 TEST(UploadRequest, MultipleMethodsSingleAuthor)
 {
-	string request = "upld 0?0?MyLicense?MyProject?MyUrl?Owner?owner@mail.com?0\na6aa62503e2ca3310e3a837502b80df5?Method1?MyProject/Method1.cpp?1?1?Owner?owner@mail.com\nf3a258ba6cd26c1b7d553a493c614104?Method2?MyProject/Method2.cpp?11?1?Owner?owner@mail.com\n59bf62494932580165af0451f76be3e9?Method3?MyProject/Method3.cpp?31?1?Owner?owner@mail.com";
+	std::string requestType = "upld";
+	std::string request = "0?0?MyLicense?MyProject?MyUrl?Owner?owner@mail.com?0\na6aa62503e2ca3310e3a837502b80df5?Method1?MyProject/Method1.cpp?1?1?Owner?owner@mail.com\nf3a258ba6cd26c1b7d553a493c614104?Method2?MyProject/Method2.cpp?11?1?Owner?owner@mail.com\n59bf62494932580165af0451f76be3e9?Method3?MyProject/Method3.cpp?31?1?Owner?owner@mail.com";
 
 	RequestHandler handler;
 	MockDatabase database;
@@ -51,14 +95,19 @@ TEST(UploadRequest, MultipleMethodsSingleAuthor)
         method3.lineNumber = 31;
         method3.authors = {author};
 
-	EXPECT_CALL(database, AddProject(project))
+	//EXPECT_CALL(database, AddProject(FieldsAre(0, 0, Eq("MyLicense"), Eq("MyProject"), "MyUrl", FieldsAre("Owner", "owner@mail.com"), 0, {"a6aa62503e2ca3310e3a837502b80df5", "f3a258ba6cd26c1b7d553a493c614104"})))
+	EXPECT_CALL(database, AddProject(projectEqual(project)))
 		.Times(1);
-	EXPECT_CALL(database, AddMethod(method1, project))
+	EXPECT_CALL(database, AddMethod(methodEqual(method1), projectEqual(project)));
+	EXPECT_CALL(database, AddMethod(ownersEqual(method1.authors), testing::_))
 		.Times(1);
-	EXPECT_CALL(database, AddMethod(method2, project))
+	EXPECT_CALL(database, AddMethod(methodEqual(method2), projectEqual(project)));
+	EXPECT_CALL(database, AddMethod(ownersEqual(method2.authors), testing::_))
 		.Times(1);
-	EXPECT_CALL(database, AddMethod(method3, project))
+	EXPECT_CALL(database, AddMethod(methodEqual(method3), projectEqual(project)));
+	EXPECT_CALL(database, AddMethod(ownersEqual(method3.authors), testing::_))
 		.Times(1);
 
-	handler.handleRequest(request)_StrEq("Your project is successfully added to the database.");
+	string result = handler.handleRequest(requestType, request);
+	ASSERT_EQ(result, "Your project is successfully added to the database.");
 }
