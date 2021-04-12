a426f7d2b700e25677d5d00843d9726f58c94254
Joël Ganesh <j.ganesh@students.uu.nl>
2021-04-12 15:18:03 +0200

    Changed RequestHandler_test.cpp according to code standards.

--- Database-API_Test/RequestHandler_test.cpp
+++ Database-API_Test/RequestHandler_test.cpp
@@ -2,6 +2,7 @@
 #include "DatabaseMock.cpp"
 #include <gtest/gtest.h>
 
+// Tests if the RequestHandler requests to connect to the database when initialized.
 TEST(GeneralTest, InitializeTest){
 	RequestHandler handler;
 	MockDatabase database;
@@ -10,6 +11,7 @@ TEST(GeneralTest, InitializeTest){
 	handler.initialize(&database);
 }
 
+// Tests if the RequestHandler correctly responds to an unknown request.
 TEST(SimpleTest, BasicAssertions){
 	RequestHandler handler;
 	EXPECT_EQ(handler.handleRequest("kill", ""), "Your input is not recognised.");
