2989afaa22044766d3bced9d7b35e6fb997576c7
Ward <w.i.j.jousma@students.uu.nl>
2021-05-10 13:44:59 +0200

    Added files and shuffled some files around

---
+++ General/Database-API.cpp
@@ -0,0 +1,26 @@
+﻿/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
+#include <iostream>
+#include <unistd.h>
+#include "Types.h"
+#include "Database-API.h"
+#include "DatabaseHandler.h"
+#include "ConnectionHandler.h"
+
+using namespace std;
+
+int main()
+{
+	usleep(45000000);
+	cout << "Connecting now" << endl;
+
+	ConnectionHandler listen;
+	DatabaseHandler databaseHandler;
+	listen.startListen(&databaseHandler);
+
+	return 0;
+}
