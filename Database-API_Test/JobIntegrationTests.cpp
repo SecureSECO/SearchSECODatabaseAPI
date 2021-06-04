978ff14ba1d2240803e9f4018b6053bce39d3ad7
Joël Ganesh <j.ganesh@students.uu.nl>
2021-06-04 19:58:27 +0200

    Another try to fix the issue with building.

--- Database-API_Test/JobIntegrationTests.cpp
+++ Database-API_Test/JobIntegrationTests.cpp
@@ -16,6 +16,9 @@ Utrecht University within the Software Project course.
 #include <string>
 #include <vector>
 
+std::string fieldDel(1, FIELD_DELIMITER_CHAR);
+std::string entryDel(1, ENTRY_DELIMITER_CHAR);
+
 // Test if first crawl is returned and then the correct job.
 TEST(JobDatabaseIntegrationTest, GetJobRequest)
 {
@@ -27,13 +30,13 @@ TEST(JobDatabaseIntegrationTest, GetJobRequest)
 	handler.initialize(&database, &jddatabase, &raftConsensus, "127.0.0.1", 9042);
 
 	std::string input = "";
-	std::string expectedOutput = "Crawl" + fieldDelimiter + "0";
+	std::string expectedOutput = "Crawl" + fieldDel + "0";
 
 	// Test:
 	std::string output = handler.handleRequest("gtjb", input, nullptr);
 	ASSERT_EQ(output, HTTPStatusCodes::success(expectedOutput));
 
-	std::string expectedOutput2 = "Spider" + fieldDelimiter + "https://github.com/caged/microsis";
+	std::string expectedOutput2 = "Spider" + fieldDel + "https://github.com/caged/microsis";
 
 	std::string output2 = handler.handleRequest("gtjb", input, nullptr);
 	ASSERT_EQ(output2, HTTPStatusCodes::success(expectedOutput2));
@@ -50,19 +53,19 @@ TEST(JobDatabaseIntegrationTest, UploadJobRequest)
 	RAFTConsensus raftConsensus;
 	handler.initialize(&database, &jddatabase, &raftConsensus, "127.0.0.1", 9042);
 
-	std::string input = "https://github.com/mcostalba/Stockfish" + fieldDelimiter + "10";
+	std::string input = "https://github.com/mcostalba/Stockfish" + fieldDel + "10";
 	std::string output = handler.handleRequest("upjb", input, nullptr);
 	JobRequestHandler *jhandler = new JobRequestHandler(&raftConsensus, &handler, &jddatabase, "127.0.0.1", 9042);
 	int jobs = jhandler->numberOfJobs;
 	ASSERT_EQ(jobs, 3);
 
 	std::string input2 = "";
-	std::string expectedOutput2 = "Crawl" + fieldDelimiter + "0";
+	std::string expectedOutput2 = "Crawl" + fieldDel + "0";
 
 	std::string output2 = handler.handleRequest("gtjb", input2, nullptr);
 	ASSERT_EQ(output2, HTTPStatusCodes::success(expectedOutput2));
 
-	std::string expectedOutput3 = "Spider" + fieldDelimiter + "https://github.com/mcostalba/Stockfish";
+	std::string expectedOutput3 = "Spider" + fieldDel + "https://github.com/mcostalba/Stockfish";
 
 	std::string output3 = handler.handleRequest("gtjb", input2, nullptr);
 	ASSERT_EQ(output3, HTTPStatusCodes::success(expectedOutput3));
@@ -78,8 +81,8 @@ TEST(JobDatabaseIntegrationTest, UploadMulitpleJobs)
 	RAFTConsensus raftConsensus;
 	handler.initialize(&database, &jddatabase, &raftConsensus, "127.0.0.1", 9042);
 
-	std::string input = "https://github.com/HackerPoet/Chaos-Equations" + fieldDelimiter + "42" + entryDelimiter +
-						"https://github.com/Yiziwinnie/Home-Depot" + fieldDelimiter + "69";
+	std::string input = "https://github.com/HackerPoet/Chaos-Equations" + fieldDel + "42" + entryDel +
+						"https://github.com/Yiziwinnie/Home-Depot" + fieldDel + "69";
 	std::string output = handler.handleRequest("upjb", input, nullptr);
 	JobRequestHandler *jhandler = new JobRequestHandler(&raftConsensus, &handler, &jddatabase, "127.0.0.1", 9042);
 	int jobs = jhandler->numberOfJobs;
@@ -97,12 +100,12 @@ TEST(JobDatabaseIntegrationTest, CrawlDataRequest)
 	handler.initialize(&database, &jddatabase, &raftConsensus, "127.0.0.1", 9042);
 
 	std::string input =
-		"100" + entryDelimiter + "https://github.com/Yiziwinnie/Bike-Sharing-in-Boston" + fieldDelimiter + "420";
+		"100" + entryDel + "https://github.com/Yiziwinnie/Bike-Sharing-in-Boston" + fieldDel + "420";
 	std::string output = handler.handleRequest("upcd", input, nullptr);
 
 	std::string input2 = "";
 	std::string output2 = handler.handleRequest("gtjb", input2, nullptr);
-	std::string expectedOutput2 = "Crawl" + fieldDelimiter + "100";
+	std::string expectedOutput2 = "Crawl" + fieldDel + "100";
 
 	ASSERT_EQ(output2, HTTPStatusCodes::success(expectedOutput2));
 }
