1c67fd3f56079568205c697a07b7327d6cee7bf7
Joël Ganesh <j.ganesh@students.uu.nl>
2021-06-04 12:56:43 +0200

    Job integration tests updated

--- Database-API_Test/JobIntegrationTests.cpp
+++ Database-API_Test/JobIntegrationTests.cpp
@@ -27,13 +27,20 @@ TEST(JobDatabaseIntegrationTest, GetJobRequest)
 	handler.initialize(&database, &jddatabase, &raftConsensus, "127.0.0.1", 9042);
 
 	std::string input = "";
-	std::string expectedOutput = "Crawl?0";
+
+	std::vector<char> expectedChars = {};
+	Utility::appendBy(expectedChars, {"Crawl", "0"}, FIELD_DELIMITER_CHAR, ENTRY_DELIMITER_CHAR);
+	expectedChars.pop_back();
+	std::string expectedOutput(expectedChars.begin(), expectedChars.end());
 
 	// Test:
 	std::string output = handler.handleRequest("gtjb", input, nullptr);
 	ASSERT_EQ(output, HTTPStatusCodes::success(expectedOutput));
 
-	std::string expectedOutput2 = "Spider?https://github.com/caged/microsis";
+	expectedChars = {};
+	Utility::appendBy(expectedChars, {"Spider", "https://github.com/caged/microsis"}, FIELD_DELIMITER_CHAR, ENTRY_DELIMITER_CHAR);
+	expectedChars.pop_back();
+	std::string expectedOutput2(expectedChars.begin(), expectedChars.end());
 
 	std::string output2 = handler.handleRequest("gtjb", input, nullptr);
 	ASSERT_EQ(output2, HTTPStatusCodes::success(expectedOutput2));
@@ -57,12 +64,20 @@ TEST(JobDatabaseIntegrationTest, UploadJobRequest)
 	ASSERT_EQ(jobs, 3);
 
 	std::string input2 = "";
-	std::string expectedOutput2 = "Crawl?0";
+
+	std::vector<char> expectedChars = {};
+	Utility::appendBy(expectedChars, {"Crawl", "0"}, FIELD_DELIMITER_CHAR, ENTRY_DELIMITER_CHAR);
+	expectedChars.pop_back();
+	std::string expectedOutput2(expectedChars.begin(), expectedChars.end());
 
 	std::string output2 = handler.handleRequest("gtjb", input2, nullptr);
 	ASSERT_EQ(output2, HTTPStatusCodes::success(expectedOutput2));
 
-	std::string expectedOutput3 = "Spider?https://github.com/mcostalba/Stockfish";
+	expectedChars = {};
+	Utility::appendBy(expectedChars, {"Spider", "https://github.com/mcostalba/Stockfish"}, FIELD_DELIMITER_CHAR,
+					  ENTRY_DELIMITER_CHAR);
+	expectedChars.pop_back();
+	std::string expectedOutput3(expectedChars.begin(), expectedChars.end());
 
 	std::string output3 = handler.handleRequest("gtjb", input2, nullptr);
 	ASSERT_EQ(output3, HTTPStatusCodes::success(expectedOutput3));
@@ -78,7 +93,13 @@ TEST(JobDatabaseIntegrationTest, UploadMulitpleJobs)
 	RAFTConsensus raftConsensus;
 	handler.initialize(&database, &jddatabase, &raftConsensus, "127.0.0.1", 9042);
 
-	std::string input = "https://github.com/HackerPoet/Chaos-Equations?42\nhttps://github.com/Yiziwinnie/Home-Depot?69";
+	std::vector<char> inputChars = {};
+	Utility::appendBy(inputChars, {"https://github.com/HackerPoet/Chaos-Equations", "42"}, FIELD_DELIMITER_CHAR,
+					  ENTRY_DELIMITER_CHAR);
+	Utility::appendBy(inputChars, {"https://github.com/Yiziwinnie/Home-Depot", "69"}, FIELD_DELIMITER_CHAR,
+					  ENTRY_DELIMITER_CHAR);
+	std::string input(inputChars.begin(), inputChars.end());
+
 	std::string output = handler.handleRequest("upjb", input, nullptr);
 	JobRequestHandler *jhandler = new JobRequestHandler(&raftConsensus, &handler, &jddatabase, "127.0.0.1", 9042);
 	int jobs = jhandler->numberOfJobs;
@@ -95,12 +116,20 @@ TEST(JobDatabaseIntegrationTest, CrawlDataRequest)
 	RAFTConsensus raftConsensus;
 	handler.initialize(&database, &jddatabase, &raftConsensus, "127.0.0.1", 9042);
 
-	std::string input = "100\nhttps://github.com/Yiziwinnie/Bike-Sharing-in-Boston?420";
+	std::vector<char> inputChars = {};
+	Utility::appendBy(inputChars, "100", ENTRY_DELIMITER_CHAR);
+	Utility::appendBy(inputChars, {"https://github.com/Yiziwinnie/Bike-Sharing-in-Boston", "420"}, FIELD_DELIMITER_CHAR,
+					  ENTRY_DELIMITER_CHAR);
+	std::string input(inputChars.begin(), inputChars.end());
 	std::string output = handler.handleRequest("upcd", input, nullptr);
 
 	std::string input2 = "";
 	std::string output2 = handler.handleRequest("gtjb", input2, nullptr);
-	std::string expectedOutput2 = "Crawl?100";
+
+	std::vector<char> expectedChars = {};
+	Utility::appendBy(expectedChars, {"Crawl", "100"}, FIELD_DELIMITER_CHAR, ENTRY_DELIMITER_CHAR);
+	expectedChars.pop_back();
+	std::string expectedOutput2(expectedChars.begin(), expectedChars.end());
 
 	ASSERT_EQ(output2, HTTPStatusCodes::success(expectedOutput2));
 }
