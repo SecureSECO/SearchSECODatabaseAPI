5c5539095ac62250bc4045d447a6bf398b897473
Joël Ganesh <j.ganesh@students.uu.nl>
2021-06-05 12:40:25 +0200

    Fixed indentation issues throughout the files.

--- Database-API_Test/HTTPStatus_test.cpp
+++ Database-API_Test/HTTPStatus_test.cpp
@@ -10,108 +10,108 @@ Utrecht University within the Software Project course.
 // Success.
 TEST(HTTPsuccesMessage, regularString)
 {
-    std::string message = "This is a normal message.";
-    std::string expected = std::string("200") + ENTRY_DELIMITER_CHAR + message;
+	std::string message = "This is a normal message.";
+	std::string expected = std::string("200") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::success(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::success(message), expected);
 }
 
 TEST(HTTPsuccesMessage, emptyString)
 {
-    std::string message = "";
+	std::string message = "";
 	std::string expected = std::string("200") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::success(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::success(message), expected);
 }
 
 TEST(HTTPsuccesMessage, oddCharacters)
 {
-    std::string message = "これは例文です。正確に表示されたら嬉しいです。";
+	std::string message = "これは例文です。正確に表示されたら嬉しいです。";
 	std::string expected = std::string("200") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::success(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::success(message), expected);
 }
 
 // ClientError.
 TEST(HTTPclientMessage, regularString)
 {
-    std::string message = "This is a normal message.";
+	std::string message = "This is a normal message.";
 	std::string expected = std::string("400") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::clientError(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::clientError(message), expected);
 }
 
 TEST(HTTPclientMessage, emptyString)
 {
-    std::string message = "";
+	std::string message = "";
 	std::string expected = std::string("400") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::clientError(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::clientError(message), expected);
 }
 
 TEST(HTTPclientMessage, oddCharacters)
 {
-    std::string message = "これは例文です。正確に表示されたら嬉しいです。";
+	std::string message = "これは例文です。正確に表示されたら嬉しいです。";
 	std::string expected = std::string("400") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::clientError(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::clientError(message), expected);
 }
 
 // ServerError.
 TEST(HTTPServerMessage, regularString)
 {
-    std::string message = "This is a normal message.";
+	std::string message = "This is a normal message.";
 	std::string expected = std::string("500") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::serverError(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::serverError(message), expected);
 }
 
 TEST(HTTPServerMessage, emptyString)
 {
-    std::string message = "";
+	std::string message = "";
 	std::string expected = std::string("500") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::serverError(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::serverError(message), expected);
 }
 
 TEST(HTTPServerMessage, oddCharacters)
 {
-    std::string message = "これは例文です。正確に表示されたら嬉しいです。";
+	std::string message = "これは例文です。正確に表示されたら嬉しいです。";
 	std::string expected = std::string("500") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::serverError(message), expected);
+	EXPECT_EQ(HTTPStatusCodes::serverError(message), expected);
 }
 
 // GetCode.
 TEST(HTTPgetCode, regularRequest)
 {
-    std::string code = "200";
+	std::string code = "200";
 	std::string request = code + ENTRY_DELIMITER_CHAR + "This is a normal message.";
 
-    EXPECT_EQ(HTTPStatusCodes::getCode(request), code);
+	EXPECT_EQ(HTTPStatusCodes::getCode(request), code);
 }
 
 TEST(HTTPgetCode, noCode)
 {
-    std::string code = "";
+	std::string code = "";
 	std::string request = code + ENTRY_DELIMITER_CHAR + "This is a normal message.";
 
-    EXPECT_EQ(HTTPStatusCodes::getCode(request), "500"); 
+	EXPECT_EQ(HTTPStatusCodes::getCode(request), "500"); 
 }
 
 // GetMessage
 TEST(HTTPgetMessage, regularRequest)
 {
-    std::string message = "This is a normal message.";
+	std::string message = "This is a normal message.";
 	std::string request = std::string("200") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::getMessage(request), message);
+	EXPECT_EQ(HTTPStatusCodes::getMessage(request), message);
 }
 
 TEST(HTTPgetMessage, noCode)
 {
-    std::string message = "";
+	std::string message = "";
 	std::string request = std::string("200") + ENTRY_DELIMITER_CHAR + message;
 
-    EXPECT_EQ(HTTPStatusCodes::getMessage(request), message);
+	EXPECT_EQ(HTTPStatusCodes::getMessage(request), message);
 }
