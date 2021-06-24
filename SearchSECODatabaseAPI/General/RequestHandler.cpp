381dc8e509a23305d850352999e20d35d2e838ee
Joël Ganesh <j.ganesh@students.uu.nl>
2021-06-24 16:23:05 +0200

    Changed folder structure.

---
+++ SearchSECODatabaseAPI/General/RequestHandler.cpp
@@ -0,0 +1,130 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
+#include "HTTPStatus.h"
+#include "RequestHandler.h"
+
+void RequestHandler::initialize(DatabaseHandler *databaseHandler, DatabaseConnection *databaseConnection,
+								RAFTConsensus *raft, std::string ip, int port)
+{
+	// Initialise the requestHandlers.
+	dbrh = new DatabaseRequestHandler(databaseHandler, ip, port);
+	jrh = new JobRequestHandler(raft, this, databaseConnection, ip, port);
+}
+
+std::string RequestHandler::handleRequest(std::string requestType, std::string request,
+										  boost::shared_ptr<TcpConnection> connection)
+{
+	ERequestType eRequest = getERequestType(requestType);
+
+	// Handle the request based on its type.
+	std::string result;
+	switch (eRequest)
+	{
+	case eUpload:
+		result = dbrh->handleUploadRequest(request);
+		break;
+	case eCheck:
+		result = dbrh->handleCheckRequest(request);
+		break;
+	case eCheckUpload:
+		result = dbrh->handleCheckUploadRequest(request);
+		break;
+	case eConnect:
+		result = jrh->handleConnectRequest(connection, request);
+		break;
+	case eUploadJob:
+		result = jrh->handleUploadJobRequest(requestType, request);
+		break;
+	case eUploadCrawlData:
+		result = jrh->handleCrawlDataRequest(requestType, request);
+		break;
+	case eGetTopJob:
+		result = jrh->handleGetJobRequest(requestType, request);
+		break;
+	case eExtractProjects:
+		result = dbrh->handleExtractProjectsRequest(request);
+		break;
+	case eGetAuthor:
+		result = dbrh->handleGetAuthorRequest(request);
+		break;
+	case eGetMethodByAuthor:
+		result = dbrh->handleGetMethodsByAuthorRequest(request);
+		break;
+	case eGetPrevProjectsRequest:
+		result = dbrh->handlePrevProjectsRequest(request);
+		break;
+	case eUnknown:
+		result = handleUnknownRequest();
+		break;
+	default:
+		result = handleNotImplementedRequest();
+		break;
+	}
+	return result;
+}
+
+std::string RequestHandler::handleUnknownRequest()
+{
+	return HTTPStatusCodes::clientError("Unknown request type.");
+}
+
+std::string RequestHandler::handleNotImplementedRequest()
+{
+	return HTTPStatusCodes::clientError("Request not implemented yet.");
+}
+
+ERequestType RequestHandler::getERequestType(std::string requestType)
+{
+	if (requestType == "upld")
+	{
+		return eUpload;
+	}
+	else if (requestType == "chck")
+	{
+		return eCheck;
+	}
+	else if (requestType == "chup")
+	{
+		return eCheckUpload;
+	}
+	else if (requestType == "conn")
+	{
+		return eConnect;
+	}
+	else if (requestType == "upjb")
+	{
+		return eUploadJob;
+	}
+	else if (requestType == "upcd")
+	{
+		return eUploadCrawlData;
+	}
+	else if (requestType == "gtjb")
+	{
+		return eGetTopJob;
+	}
+	else if (requestType == "extp")
+	{
+		return eExtractProjects;
+	}
+	else if (requestType == "idau")
+	{
+		return eGetAuthor;
+	}
+	else if (requestType == "aume")
+	{
+		return eGetMethodByAuthor;
+	}
+	else if (requestType == "gppr")
+	{
+		return eGetPrevProjectsRequest;
+	}
+	else
+	{
+		return eUnknown;
+	}
+}
---
+++ tests/ConnectionHandler_tests.cpp
@@ -0,0 +1,182 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
+#include "ConnectionHandler.h"
+#include "Database-API.h"
+#include "DatabaseHandler.h"
+#include "Definitions.h"
+#include "ConnectionHandler.h"
+#include "Networking.h"
+#include "RAFTConsensus.h"
+#include "RequestHandlerMock.cpp"
+
+#include <gtest/gtest.h>
+#include <thread>
+
+#define TESTIP "127.0.0.1"
+
+MATCHER_P(notnullptrMatcher, request, "") 
+{
+	return arg != nullptr;
+}
+
+TEST(ConnectionHandlerIntegrationTests, basic_request)
+{
+	// Set up the test.
+	errno = 0;
+
+	const int TESTCONNECTPORT = 9044;
+	std::string entryDelimiter(1, ENTRY_DELIMITER_CHAR);
+	std::thread *thread;
+
+	RequestHandlerMock handler;
+	ConnectionHandler listen;
+
+	const std::string input = "2c7f46d4f57cf9e66b03213358c7ddb5" + entryDelimiter;
+
+	std::vector<char> outputChars = {};
+	Utility::appendBy(outputChars,
+					  {"2c7f46d4f57cf9e66b03213358c7ddb5", "1", "5000000000000", "M1", "P1/M1.cpp", "1", "1",
+					   "68bd2db6-fe91-47d2-a134-cf82b104f547"},
+					  FIELD_DELIMITER_CHAR, ENTRY_DELIMITER_CHAR);
+
+	const std::string expectedOutput = std::string(outputChars.begin(), outputChars.end());
+
+	// We expect the following call towards the request handler.
+	EXPECT_CALL(handler, handleRequest("chck", input, notnullptrMatcher(nullptr)))
+		.WillOnce(testing::Return(expectedOutput));
+
+	thread = new std::thread(&ConnectionHandler::startListen, &listen, nullptr, nullptr, nullptr, TESTCONNECTPORT, 
+							 &handler);
+	usleep(500000); // Just to make sure the listener has started.
+
+	NetworkHandler *networkHandler = NetworkHandler::createHandler();
+	networkHandler->openConnection(TESTIP, std::to_string(TESTCONNECTPORT));
+	networkHandler->sendData("chck" + std::to_string(input.size()) + entryDelimiter);
+	networkHandler->sendData(input);
+
+	// Check if the output is as expected.
+	std::string result = networkHandler->receiveData(false);
+	ASSERT_EQ(result, expectedOutput);
+	
+	delete networkHandler;
+	thread->join();
+	delete thread;
+}
+
+TEST(ConnectionHandlerIntegrationTests, basic_in_chunks)
+{
+	// Set up the test.
+	errno = 0;
+
+	const int TESTCONNECTPORT = 9045;
+	std::string entryDelimiter(1, ENTRY_DELIMITER_CHAR);
+	std::thread *thread;
+
+	RequestHandlerMock handler;
+	ConnectionHandler listen;
+
+	const std::string input = "2c7f46d4f57cf9e66b03213358c7ddb5" + entryDelimiter;
+
+	std::vector<char> outputChars = {};
+
+	Utility::appendBy(outputChars,
+					  {"2c7f46d4f57cf9e66b03213358c7ddb5", "1", "5000000000000", "M1", "P1/M1.cpp", "1", "1",
+					   "68bd2db6-fe91-47d2-a134-cf82b104f547"},
+					  FIELD_DELIMITER_CHAR, ENTRY_DELIMITER_CHAR);
+	const std::string expectedOutput = std::string(outputChars.begin(), outputChars.end());
+	// Mock expectations.
+	EXPECT_CALL(handler, handleRequest("chck", input, notnullptrMatcher(nullptr)))
+		.Times(1)
+		.WillOnce(testing::Return(expectedOutput));
+
+	thread = new std::thread(&ConnectionHandler::startListen, &listen, nullptr, nullptr, nullptr, TESTCONNECTPORT, 
+							 &handler);
+	usleep(500000); // Just to make sure the listner has started.
+
+	NetworkHandler *networkHandler = NetworkHandler::createHandler();
+	networkHandler->openConnection(TESTIP, std::to_string(TESTCONNECTPORT));
+	networkHandler->sendData("chck" + std::to_string(input.size()) + entryDelimiter);
+	networkHandler->sendData(input.substr(0, 12));
+	usleep(500000);
+	networkHandler->sendData(input.substr(12));
+
+	// Check if the output is as expected.
+	std::string result = networkHandler->receiveData(false);
+	ASSERT_EQ(result, expectedOutput);
+
+	delete networkHandler;
+	thread->join();
+	delete thread;
+}
+
+TEST(ConnectionHandlerIntegrationTests, too_big_request)
+{
+	// Set up the test.
+	errno = 0;
+
+	const int TESTCONNECTPORT = 9046;
+	std::string entryDelimiter(1, ENTRY_DELIMITER_CHAR);
+	std::thread *thread;
+
+	RequestHandlerMock handler;
+	ConnectionHandler listen;
+
+	const std::string input = "2c7f46d4f57cf9e66b03213358c7ddb5" + entryDelimiter;
+
+	const std::string expectedOutput = "Request body larger than expected.";
+
+	thread = new std::thread(&ConnectionHandler::startListen, &listen, nullptr, nullptr, nullptr, TESTCONNECTPORT, 
+							 &handler);
+	usleep(500000); // Just to make sure the listener has started.
+
+	NetworkHandler *networkHandler = NetworkHandler::createHandler();
+	networkHandler->openConnection(TESTIP, std::to_string(TESTCONNECTPORT));
+	networkHandler->sendData("chck" + std::to_string(input.size() - 10) + entryDelimiter);
+	networkHandler->sendData(input);
+
+	// Check if the output is as expected.
+	std::string result = networkHandler->receiveData(false);
+	ASSERT_EQ(result, expectedOutput);
+
+	delete networkHandler;
+	thread->join();
+	delete thread;
+}
+
+TEST(ConnectionHandlerIntegrationTests, invalid)
+{
+	// Set up the test.
+	errno = 0;
+
+	const int TESTCONNECTPORT = 9047;
+	std::string entryDelimiter(1, ENTRY_DELIMITER_CHAR);
+	std::thread *thread;
+
+	RequestHandlerMock handler;
+	ConnectionHandler listen;
+
+	const std::string input = "2c7f46d4f57cf9e66b03213358c7ddb5" + entryDelimiter;
+
+	const std::string expectedOutput = "Error parsing command.";
+
+	thread = new std::thread(&ConnectionHandler::startListen, &listen, nullptr, nullptr, nullptr, TESTCONNECTPORT, 
+							 &handler);
+	usleep(500000); // Just to make sure the listner has started.
+
+	NetworkHandler *networkHandler = NetworkHandler::createHandler();
+	networkHandler->openConnection(TESTIP, std::to_string(TESTCONNECTPORT));
+	networkHandler->sendData("chckk" + entryDelimiter);
+	networkHandler->sendData(input);
+
+	// Check if the output is correct.
+	std::string result = networkHandler->receiveData(false);
+	ASSERT_EQ(result, expectedOutput);
+
+	delete networkHandler;
+	thread->join();
+	delete thread;
+}
