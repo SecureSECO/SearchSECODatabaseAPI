c1015f153c88b901b66a8526b17098b86e5d8edc
Marien <matser71@gmail.com>
2021-07-15 11:13:42 +0200

    Fix typing mistake

--- SearchSECODatabaseAPI/JobDistribution/DatabaseConnection.h
+++ SearchSECODatabaseAPI/JobDistribution/DatabaseConnection.h
@@ -44,7 +44,7 @@ public:
 	virtual int getCrawlID();
 
 	/// <summary>
-	/// Sets the craw ID in the database to the given value.
+	/// Sets the crawl ID in the database to the given value.
 	/// </summary>
 	virtual void setCrawlID(int id);
 
